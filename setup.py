0b03e0fb963e495cd1f3c6498373179143250ae5
Shengyang Shi <shengyang.shi@hotmail.com>
2017-06-25 21:24:19 -0700

    updated dependencies on pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 setup(name="KNN-attackToolBox",
-	  version="0.1",
+	  version="0.2",
 	  description="Attack tool box for KNN algo",
 	  url="https://github.com/cwscx/attackToolBox",
 	  author="Shengyang Shi",
 	  author_email="shengyang.shi@hotmail.com",
 	  license="MIT",
 	  packages=["attackToolBox"],
+	  install_requires=[
+	  	"numpy",
+	  	"tensorflow-gpu",
+	  	"scikit-learn",
+	  	"matplotlib"
+	  ],
 	  zip_safe=False)
