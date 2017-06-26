389228d7dddb440c184db112ebb009c489eafa8b
Shengyang Shi <shengyang.shi@hotmail.com>
2017-06-25 20:58:16 -0700

    add basic setup script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # attackToolBox
 Black box Attack Tool Box for KNN
+
+pip install [-e] .
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="KNN-attackToolBox",
+	  version="0.1",
+	  description="Attack tool box for KNN algo",
+	  url="https://github.com/cwscx/attackToolBox",
+	  author="Shengyang Shi",
+	  author_email="shengyang.shi@hotmail.com",
+	  license="MIT",
+	  packages=["attackToolBox"],
+	  zip_safe=False)
