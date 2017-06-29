226b46aff1d1f3443ae6806e68b323c85199e86e
Shengyang Shi <shengyang.shi@hotmail.com>
2017-06-29 02:10:55 -0700

    add tests

---
+++ example/NNtest.py
@@ -0,0 +1,12 @@
+from attackToolBox import *
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+atb = attackToolBox(mnist.train.images[:20000], mnist.train.labels[:20000])
+print("finish initializing")
+atb.polluteByNN(mnist.test.images[:2000], mnist.test.labels[:2000], 0.01, 50)
+print("finish polluting")
+atb.savePollutedImages()
+atb.testKNN()
+atb.showPollutedImage(20)
